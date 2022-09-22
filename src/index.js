d1b04956e1ba485d0033d3a5a1b9cf4e4fcadf94
João Gabriel <joaogabrieloliveira81@gmail.com>
2022-09-21 21:48:17 -0300

    feat: criando conexao com banco de dados

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 import axios from "axios";
 import Login, { Logout } from "./service/login.js"
+
+import "reflect-metadata";
+import typeorm from 'typeorm';
+
 //Esse import é a mesma coisa de usar o require
 //const express = require("express");
 
@@ -24,6 +28,24 @@ app.get("/login", (req, res) => {
 });
 
 app.get("/logout", Logout)
+
+var dataSource = new typeorm.DataSource({
+    type:"sqlite",
+    database: "database/database.sqlite",
+    logger: true,
+    entities: [
+      "models/*.js"
+    ]
+}); 
+
+
+dataSource.initialize()
+.then(() => {
+    console.log("Banco de dados foi iniciado com sucesso!")
+})
+.catch((err) => {
+    console.error("Aconteceu um erro ao inciar banco de dados", err)
+});
 
 app.listen(PORT, () => {
     console.log("Iniciando HealthStock Login");
