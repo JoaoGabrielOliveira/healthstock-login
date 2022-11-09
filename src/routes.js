365281c8a715784108546512a18e178da094c22f
João Gabriel <joaogabrieloliveira81@gmail.com>
2022-11-08 21:22:32 -0300

    fix: bug com rotas

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import express from 'express';
 import Login from './service/login.js';
 import { getUser, getAllUser, deleteUser, saveUser, updateUser } from "./service/user.js";
 import { saveRegistrationBuyer, updateRegistrationBuyer } from "./service/registrationBuyer.js";
-import { getRegistrationSupplier, getAllRegistrationSupplier, saveRegistrationSupplier, updateRegistrationSupplier } from "./service/registrationSupplier.js";
+import { saveRegistrationSupplier, updateRegistrationSupplier } from "./service/registrationSupplier.js";
 
 const router = express.Router();
 
@@ -20,8 +20,6 @@ router.post("/cadastro/comprador", saveRegistrationBuyer);
 router.put("/cadastro/comprador", updateRegistrationBuyer);
 
 router.post("/cadastro/fornecedor", saveRegistrationSupplier);
-router.get("/cadastro/fornecedor", getAllRegistrationSupplier);
-router.get("/cadastro/:id/fornecedor", getRegistrationSupplier);
 router.put("/cadastro/fornecedor", updateRegistrationSupplier);
 
 export default router;
