546ad48f113037217d1715fd1e47889ba4edf245
João Gabriel <joaogabrieloliveira81@gmail.com>
2022-11-13 16:42:25 -0300

    refactor: add envio de mensagem para barramento e melhorando login

--- src/service/login.js
+++ src/service/login.js
@@ -1,6 +1,12 @@
 import User from "../models/user.js";
 import Supplier from '../models/supplier.js';
 import Buyer from '../models/buyer.js';
+import Contact from '../models/contact.js';
+import Address from '../models/Address.js';
+
+import { IsNull } from 'typeorm';
+
+import { SendEvent } from '../config/index.js';
 
 //Export default é para exportar a função em outros arquivos.
 //Função para logar
@@ -11,12 +17,16 @@ export default async function Login(req, res, next) {
     let password = loginBody.password;
 
     if(!email) {
-        res.status(401).send({error:"Email não foi enviado na requisição!"});
+        const message = "Email não foi enviado na requisição!";
+        SendEvent(message, email, 'warn');
+        res.status(401).send({message:message});
         return;
     }
 
     if(!password) {
-        res.status(401).send({error:"Senha não foi enviado na requisição!"});
+        const message = "Senha não foi enviado na requisição!";
+        SendEvent(message, email, 'warn');
+        res.status(401).send({message:message});
         return;
     }
     let user;
@@ -25,14 +35,18 @@ export default async function Login(req, res, next) {
         
 
         if(!user){
-            res.status(401).send({error:"Email não encontrado!"});
+            const message = "Email não encontrado!";
+            SendEvent(message, user, 'warn');
+            res.status(401).send({message:message});
             return;
         }
 
         let checkPass = await checkPassword(password, user.password);
 
         if(!checkPass){
-            res.status(401).send({error:"Senha está incorreta!"});
+            const message = "Senha está incorreta!";
+            SendEvent(message, user, 'warn');
+            res.status(401).send({message:message});
             return;
         }
 
@@ -41,15 +55,18 @@ export default async function Login(req, res, next) {
         let userType = await checkTypeOfUser(user.id);
 
         if(!userType){
-            res.status(401).send({error: "Usuario não está com dados completos.Usuários não tem dados informando se é Comprador ou Fornecedor"});
+            const message = "Usuario não está com dados completos.Usuários não tem dados informando se é Comprador ou Fornecedor";
+            SendEvent(message, user, 'warn');
+            res.status(401).send({message:message});
             return;
         }
 
         userType.userType.user = user;
 
-
+        SendEvent('Login realizado com sucesso!', userType);
         res.status(202).send(userType);
     } catch (err) {
+        SendEvent('Aconteceu um erro inesperado!', [user, err], 'error');
         res.status(500).send({message: "Aconteceu um erro inesperado", error: err.message});
     }
     finally{
@@ -64,15 +81,26 @@ async function checkPassword(plainPassword, hashPassword){
 }
 
 async function checkTypeOfUser(userId){
-    let userType = await Supplier.findOneBy({user: {id: userId}});
+    const where = {user: {id: userId}};
+
+    let userType = await Supplier.findOne({where: where});
+
+    Address.getRepository().count();
 
     if(userType){
+        userType.contacts = await Contact.findBy({supplier: {id: userType.id}, buyer: IsNull()});
+        
+        userType.addresses = await Address.findBy({supplier: {id: userType.id}, buyer: IsNull()});
+
         return {type: "supplier", userType};
     }
 
-    userType = await Buyer.findOneBy({ user: {id: userId}});
+    userType = await Buyer.findOne({where: where});
 
     if(userType){
+        userType.contacts = await Contact.findBy({buyer: {id: userType.id}, supplier: IsNull()});
+        userType.addresses = await Address.findBy({buyer: {id: userType.id}, supplier: IsNull()});
+
         return {type: "buyer", userType};
     }
 
